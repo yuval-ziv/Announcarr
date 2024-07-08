34d451dad33b953cd231183c771a0994be5b2876
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-07-09 01:02:13 +0300

    added radarr integration

---
+++ Integrations/Radarr/Radarr/Configurations/RadarrIntegrationConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Announcer.Integrations.Radarr.Configurations;
+
+public class RadarrIntegrationConfiguration
+{
+    public string Url { get; set; } = "http://localhost:7878";
+    public string? ApiKey { get; set; }
+    public bool IgnoreCertificateValidation { get; set; } = false;
+}
