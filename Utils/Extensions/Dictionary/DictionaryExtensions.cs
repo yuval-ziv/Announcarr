ac2cb4b0fd3dcab97903015099e897446c0e434f
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-07-09 01:00:42 +0300

    added dictionary select extension method that accepts a key value selector

---
+++ Utils/Extensions/Dictionary/DictionaryExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Announcer.Utils.Extensions.Dictionary;
+
+public static class DictionaryExtensions
+{
+    public static IEnumerable<TResult> Select<TSourceKey, TSourceValue, TResult>(this IDictionary<TSourceKey, TSourceValue> dictionary, Func<TSourceKey, TSourceValue, TResult> selector)
+        where TSourceKey : notnull
+    {
+        return dictionary.Select(kvp => selector(kvp.Key, kvp.Value));
+    }
+}
