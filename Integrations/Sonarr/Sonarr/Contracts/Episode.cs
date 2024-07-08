1a8d83b0d9e010c6b301c25c57827bd4363971c8
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-07-09 01:02:24 +0300

    added sonarr integration

---
+++ Integrations/Sonarr/Sonarr/Contracts/Episode.cs
@@ -0,0 +1,7 @@
+﻿namespace Announcer.Integrations.Sonarr.Contracts;
+
+public class Episode
+{
+    public int EpisodeNumber { get; set; }
+    public string? EpisodeTitle { get; set; }
+}
---
+++ Integrations/Sonarr/Sonarr/Contracts/Season.cs
@@ -0,0 +1,7 @@
+﻿namespace Announcer.Integrations.Sonarr.Contracts;
+
+public class Season
+{
+    public int SeasonNumber { get; set; }
+    public List<Episode> Episodes { get; set; } = [];
+}
