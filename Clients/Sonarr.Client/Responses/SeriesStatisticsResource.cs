03e4458e044d418ff85fb3c10df9eebce968d1dd
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-07-22 00:43:42 +0300

    fix SizeOnDisk type

--- Clients/Sonarr.Client/Responses/SeriesStatisticsResource.cs
+++ Clients/Sonarr.Client/Responses/SeriesStatisticsResource.cs
@@ -6,7 +6,7 @@ public class SeriesStatisticsResource
     public int EpisodeFileCount { get; set; }
     public int EpisodeCount { get; set; }
     public int TotalEpisodeCount { get; set; }
-    public int SizeOnDisk { get; set; }
+    public long? SizeOnDisk { get; set; }
     public List<string>? ReleaseGroups { get; set; }
     public double PercentOfEpisodes { get; set; }
 }
