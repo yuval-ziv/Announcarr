282384c0bb9493fdb4a0873097a49f67daa3074d
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-07-21 23:31:23 +0300

    added option to skip null values when using WithQueryParameters

--- Utils/Extensions/Dictionary/DictionaryExtensions.cs
+++ Utils/Extensions/Dictionary/DictionaryExtensions.cs
@@ -2,9 +2,30 @@
 
 public static class DictionaryExtensions
 {
-    public static IEnumerable<TResult> Select<TSourceKey, TSourceValue, TResult>(this IDictionary<TSourceKey, TSourceValue> dictionary, Func<TSourceKey, TSourceValue, TResult> selector)
+    public static IEnumerable<TResult> Select<TSourceKey, TSourceValue, TResult>(this IDictionary<TSourceKey, TSourceValue?> dictionary, Func<TSourceKey, TSourceValue?, TResult> selector)
         where TSourceKey : notnull
     {
         return dictionary.Select(kvp => selector(kvp.Key, kvp.Value));
+    }
+
+    public static IEnumerable<TResult> Select<TSourceKey, TSourceValue, TResult>(this IEnumerable<KeyValuePair<TSourceKey, TSourceValue?>> dictionary,
+        Func<TSourceKey, TSourceValue?, TResult> selector)
+        where TSourceKey : notnull
+    {
+        return dictionary.Select(kvp => selector(kvp.Key, kvp.Value));
+    }
+
+    public static IEnumerable<KeyValuePair<TSourceKey, TSourceValue?>> Where<TSourceKey, TSourceValue>(this IDictionary<TSourceKey, TSourceValue?> dictionary,
+        Func<TSourceKey, TSourceValue?, bool> selector)
+        where TSourceKey : notnull
+    {
+        return dictionary.Where(kvp => selector(kvp.Key, kvp.Value));
+    }
+
+    public static IEnumerable<KeyValuePair<TSourceKey, TSourceValue?>> Where<TSourceKey, TSourceValue>(this IEnumerable<KeyValuePair<TSourceKey, TSourceValue?>> dictionary,
+        Func<TSourceKey, TSourceValue?, bool> selector)
+        where TSourceKey : notnull
+    {
+        return dictionary.Where(kvp => selector(kvp.Key, kvp.Value));
     }
 }
--- Utils/Extensions/Http/StringExtensions.cs
+++ Utils/Extensions/Http/StringExtensions.cs
@@ -5,8 +5,8 @@ namespace Announcarr.Utils.Extensions.Http;
 
 public static class StringExtensions
 {
-    public static string WithQueryParameters(this string api, IDictionary<string, string> queryParameters)
+    public static string WithQueryParameters(this string api, IDictionary<string, string?> queryParameters, bool skipNullValues = true)
     {
-        return api + "?" + string.Join("&", queryParameters.Select((key, value) => key + "=" + HttpUtility.UrlEncode(value)));
+        return api + "?" + string.Join("&", queryParameters.Where((_, value) => skipNullValues || value is not null).Select((key, value) => key + "=" + HttpUtility.UrlEncode(value)));
     }
 }
