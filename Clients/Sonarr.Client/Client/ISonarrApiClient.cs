f9306f340b1c5eb1ecb7e2c39bae0fb579b2ed13
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-07-21 23:31:46 +0300

    added GetSeriesAsync api to sonarr

--- Clients/Sonarr.Client/Client/ISonarrApiClient.cs
+++ Clients/Sonarr.Client/Client/ISonarrApiClient.cs
@@ -6,4 +6,6 @@ public interface ISonarrApiClient : IDisposable
 {
     Task<List<EpisodeResource>> GetCalendarAsync(DateTimeOffset start, DateTimeOffset end, bool unmonitored = false, bool includeSeries = false, bool includeEpisodeFile = false,
         bool includeEpisodesImages = false, string tags = "", CancellationToken cancellationToken = default);
+
+    Task<List<SeriesResource>> GetSeriesAsync(int? tvdbId, bool includeSeasonImages = false, CancellationToken cancellationToken = default);
 }
