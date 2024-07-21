50b6bd02d43a1cc0f3de7bbdb7fb9b7b3d5133b8
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-07-21 23:56:28 +0300

    added DateTime extensions

---
+++ Utils/Extensions/DateTime/DateTimeExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Announcarr.Utils.Extensions.DateTime;
+
+public static class DateTimeExtensions
+{
+    public static bool Between(this DateTimeOffset current, DateTimeOffset from, DateTimeOffset to)
+    {
+        return current > from && current < to;
+    }
+
+    public static bool Between(this System.DateTime current, DateTimeOffset from, DateTimeOffset to) => new DateTimeOffset(current).Between(from, to);
+}
