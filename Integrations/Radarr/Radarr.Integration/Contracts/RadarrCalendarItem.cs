890a47cac741eddb48de0073c9b34535142f68aa
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-07-26 16:54:15 +0300

    moved ReleaseDateType.cs to its own file

--- Integrations/Radarr/Radarr.Integration/Contracts/RadarrCalendarItem.cs
+++ Integrations/Radarr/Radarr.Integration/Contracts/RadarrCalendarItem.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using Announcarr.Integrations.Abstractions.Responses;
+﻿using Announcarr.Integrations.Abstractions.Responses;
 using EnumsNET;
 
 namespace Announcarr.Integrations.Radarr.Integration.Contracts;
@@ -11,11 +10,4 @@ public class RadarrCalendarItem : BaseCalendarItem
 
     public override string? GetCaption(string dateTimeFormat) =>
         $"{MovieName}{(ReleaseDate is not null ? $" - {ReleaseDateType.AsString(EnumFormat.Description)} on {ReleaseDate?.ToString(dateTimeFormat)}" : string.Empty)}";
-}
-
-public enum ReleaseDateType
-{
-    [Description("physical release")] PhysicalRelease,
-    [Description("digital release")] DigitalRelease,
-    [Description("in cinemas")] InCinemas,
 }
---
+++ Integrations/Radarr/Radarr.Integration/Contracts/ReleaseDateType.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace Announcarr.Integrations.Radarr.Integration.Contracts;
+
+public enum ReleaseDateType
+{
+    [Description("physical release")] PhysicalRelease,
+    [Description("digital release")] DigitalRelease,
+    [Description("in cinemas")] InCinemas,
+}
