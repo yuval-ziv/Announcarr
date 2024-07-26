96d1c9874918b8da324ee4e673f5e1187d22723e
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-07-26 16:57:49 +0300

    formatting

--- Integrations/Abstractions/Abstractions.Integration/Interfaces/IIntegrationService.cs
+++ Integrations/Abstractions/Abstractions.Integration/Interfaces/IIntegrationService.cs
@@ -4,11 +4,11 @@ namespace Announcarr.Integrations.Abstractions.Interfaces;
 
 public interface IIntegrationService
 {
-    bool IsEnabled { get;  }
+    bool IsEnabled { get; }
     string GetName();
-    bool IsGetCalendarEnabled { get;  }
+    bool IsGetCalendarEnabled { get; }
     Task<CalendarResponse> GetCalendarAsync(DateTimeOffset from, DateTimeOffset to, CancellationToken cancellationToken = default);
-    bool IsGetRecentlyAddedEnabled { get;  }
+    bool IsGetRecentlyAddedEnabled { get; }
     Task<RecentlyAddedResponse> GetRecentlyAddedAsync(DateTimeOffset from, DateTimeOffset to, CancellationToken cancellationToken = default);
     Task GetNewAnnouncementAsync(DateTimeOffset from, DateTimeOffset to, CancellationToken cancellationToken = default);
 }
