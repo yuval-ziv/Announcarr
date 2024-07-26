ee0a08eb2ae3fa30ba478b0b1d32b3949f4bea69
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-07-27 02:11:31 +0300

    changed the way integrations are registered for dependency injection

--- Integrations/Abstractions/Abstractions.Inegration.Extensions/DependencyInjection/ServiceCollectionServiceExtensions.cs
+++ Integrations/Abstractions/Abstractions.Inegration.Extensions/DependencyInjection/ServiceCollectionServiceExtensions.cs
@@ -7,29 +7,34 @@ namespace Announcarr.Integrations.Abstractions.Integration.Extensions.Dependency
 
 public static class ServiceCollectionServiceExtensions
 {
-    public static IServiceCollection AddIntegration<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TImplementation>(this IServiceCollection services)
+    public static IServiceCollection AddIntegrations<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TImplementation, TConfiguration>(this IServiceCollection services)
         where TImplementation : class, IIntegrationService
+        where TConfiguration : class, IIntegrationConfiguration
     {
-        services.AddSingleton<IIntegrationService, TImplementation>();
+        using ServiceProvider serviceProvider = services.BuildServiceProvider();
+        List<TConfiguration> list = serviceProvider.GetService<IOptions<List<TConfiguration>>>()?.Value ?? [];
+
+        return services.AddIntegrations<TImplementation, TConfiguration>(list);
+    }
+
+    public static IServiceCollection AddIntegrations<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TImplementation, TConfiguration>(this IServiceCollection services,
+        List<TConfiguration>? configurations)
+        where TImplementation : class, IIntegrationService
+        where TConfiguration : class, IIntegrationConfiguration
+    {
+        foreach (TConfiguration configuration in configurations ?? Enumerable.Empty<TConfiguration>())
+        {
+            services.AddIntegration<TImplementation, TConfiguration>(configuration);
+        }
+
         return services;
     }
 
-    public static IServiceCollection WithIntegrationConfiguration<TConfiguration>(this IServiceCollection services) where TConfiguration : class, new()
+    private static IServiceCollection AddIntegration<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TImplementation, TConfiguration>(this IServiceCollection services,
+        TConfiguration configuration)
+        where TImplementation : class, IIntegrationService
+        where TConfiguration : class, IIntegrationConfiguration
     {
-        services.WithIntegrationConfiguration(provider => provider.GetService<IOptions<TConfiguration>>()?.Value ?? new TConfiguration());
-        return services;
-    }
-
-    public static IServiceCollection WithIntegrationConfiguration<TConfiguration>(this IServiceCollection services, TConfiguration configuration) where TConfiguration : class
-    {
-        services.AddSingleton(configuration);
-        return services;
-    }
-
-    public static IServiceCollection WithIntegrationConfiguration<TConfiguration>(this IServiceCollection services, Func<IServiceProvider, TConfiguration> configurationFactory)
-        where TConfiguration : class
-    {
-        services.AddSingleton(typeof(TConfiguration), configurationFactory);
-        return services;
+        return services.AddSingleton<IIntegrationService, TImplementation>(_ => (TImplementation)Activator.CreateInstance(typeof(TImplementation), configuration)!);
     }
 }
