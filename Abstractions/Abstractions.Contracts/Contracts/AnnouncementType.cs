ec6d3bc3be99cf9dc9da6ff3420f132053f1cb60
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-07-27 16:25:53 +0300

    added AnnouncementType enum

---
+++ Abstractions/Abstractions.Contracts/Contracts/AnnouncementType.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace Announcarr.Abstractions.Contracts.Contracts;
+
+public enum AnnouncementType
+{
+    Calendar,
+    [Description("Recently Added")] RecentlyAdded,
+}
