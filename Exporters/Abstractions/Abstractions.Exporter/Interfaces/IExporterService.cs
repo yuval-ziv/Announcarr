6827ee03a5c8bb0c6ee915b20a4f8800d2d7ea5f
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-07-27 21:49:37 +0300

    IExporterService now accepts an announcements enumerable, and sends only those with supported tags

--- Exporters/Abstractions/Abstractions.Exporter/Interfaces/IExporterService.cs
+++ Exporters/Abstractions/Abstractions.Exporter/Interfaces/IExporterService.cs
@@ -8,10 +8,7 @@ public interface IExporterService
     string Name { get; }
     bool? ExportOnEmptyContract { get; set; }
     string? CustomMessageOnEmptyContract { get; set; }
-    bool IsTestExporterEnabled { get; }
-    bool IsExportCalendarEnabled { get; }
-    bool IsExportRecentlyAddedEnabled { get; }
     Task TestExporterAsync(CancellationToken cancellationToken = default);
-    Task ExportCalendarAsync(CalendarContract calendarContract, DateTimeOffset startDate, DateTimeOffset endDate, CancellationToken cancellationToken = default);
-    Task ExportRecentlyAddedAsync(RecentlyAddedContract recentlyAddedContract, DateTimeOffset startDate, DateTimeOffset endDate, CancellationToken cancellationToken = default);
+    Task ExportCalendarAsync(IEnumerable<CalendarContract> calendarContracts, DateTimeOffset startDate, DateTimeOffset endDate, CancellationToken cancellationToken = default);
+    Task ExportRecentlyAddedAsync(IEnumerable<RecentlyAddedContract> recentlyAddedContracts, DateTimeOffset startDate, DateTimeOffset endDate, CancellationToken cancellationToken = default);
 }
