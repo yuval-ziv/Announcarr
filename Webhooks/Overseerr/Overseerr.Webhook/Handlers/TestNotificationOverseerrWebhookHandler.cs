8237300e98a7fc5075ec20ef8aa4af431c6053bb
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-11-16 03:48:45 +0200

    removed unused logger field and empty constructor

--- Webhooks/Overseerr/Overseerr.Webhook/Handlers/TestNotificationOverseerrWebhookHandler.cs
+++ Webhooks/Overseerr/Overseerr.Webhook/Handlers/TestNotificationOverseerrWebhookHandler.cs
@@ -1,19 +1,11 @@
 ﻿using Announcarr.Abstractions.Contracts;
 using Announcarr.Webhooks.Overseerr.Webhook.Contracts;
 using Announcarr.Webhooks.Overseerr.Webhook.Contracts.Enums;
-using Microsoft.Extensions.Logging;
 
 namespace Announcarr.Webhooks.Overseerr.Webhook.Handlers;
 
 public class TestNotificationOverseerrWebhookHandler : IOverseerrWebhookHandler
 {
-    private readonly ILogger<TestNotificationOverseerrWebhookHandler> _logger;
-
-    public TestNotificationOverseerrWebhookHandler(ILogger<TestNotificationOverseerrWebhookHandler> logger)
-    {
-        _logger = logger;
-    }
-
     public NotificationType NotificationType => NotificationType.TestNotification;
 
     public CustomAnnouncement Handle(OverseerrWebhookContract contract, CancellationToken cancellationToken = default)
