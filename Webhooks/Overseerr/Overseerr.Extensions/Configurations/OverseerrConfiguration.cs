e4669d6fb1ee856304b98740bd3ee16f3a2b6bf3
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-11-22 22:44:41 +0200

    formatting

--- Webhooks/Overseerr/Overseerr.Extensions/Configurations/OverseerrConfiguration.cs
+++ Webhooks/Overseerr/Overseerr.Extensions/Configurations/OverseerrConfiguration.cs
@@ -5,15 +5,16 @@ namespace Announcarr.Webhooks.Overseerr.Extensions.Configurations;
 public class OverseerrConfiguration
 {
     public string? Name { get; set; } = null;
+
     public bool EnableWebhookListener { get; set; } = false;
     public bool AllowRemoteRequests { get; set; } = true;
-    
+
     public string Method { get; set; } = HttpMethod.Post.Method;
     public string Path { get; set; } = "/overseerr/webhook";
     public string? AuthorizationHeader { get; set; } = string.Empty;
-    
+
     public string OverseerrUrl { get; set; } = "http://localhost:5055";
-    
+
     public bool IsEnabled { get; set; } = true;
     public Dictionary<NotificationType, NotificationTypeConfiguration> NotificationTypeToConfiguration { get; set; } = [];
 }
