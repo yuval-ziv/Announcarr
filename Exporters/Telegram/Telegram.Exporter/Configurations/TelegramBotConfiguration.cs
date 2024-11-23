232ced52c4473c12e07cae1b0ff658ae188cf33d
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-11-23 21:43:56 +0200

    moved TelegramBotConfiguration to its own file

---
+++ Exporters/Telegram/Telegram.Exporter/Configurations/TelegramBotConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace Announcarr.Exporters.Telegram.Exporter.Configurations;
+
+public class TelegramBotConfiguration
+{
+    public required string Token { get; set; }
+    public required List<long> ChatIds { get; set; } = [];
+}
--- Exporters/Telegram/Telegram.Exporter/Configurations/TelegramExporterConfiguration.cs
+++ Exporters/Telegram/Telegram.Exporter/Configurations/TelegramExporterConfiguration.cs
@@ -9,9 +9,3 @@ public class TelegramExporterConfiguration : BaseExporterConfiguration
     public string DateTimeFormat { get; set; } = "dd/MM/yyyy";
     public TelegramBotConfiguration? Bot { get; set; }
 }
-
-public class TelegramBotConfiguration
-{
-    public required string Token { get; set; }
-    public required List<long> ChatIds { get; set; } = [];
-}
