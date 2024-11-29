d9ab9788a56463b93bfc5b3c4262db89403d1e7e
Yuval Ziv <44985263+yuval-ziv@users.noreply.github.com>
2024-11-29 17:19:24 +0200

    added id to announcement

--- Abstractions/Abstractions.Contracts/Contracts/BaseAnnouncement.cs
+++ Abstractions/Abstractions.Contracts/Contracts/BaseAnnouncement.cs
@@ -2,6 +2,7 @@
 
 public abstract class BaseAnnouncement
 {
+    public Guid Id { get; set; } = Guid.CreateVersion7();
     public virtual bool IsEmpty { get; }
     public virtual AnnouncementType AnnouncementType { get; }
     public virtual HashSet<string> Tags { get; set; } = [];
